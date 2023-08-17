2bb23a8e0e5ca5dcf8b19ef79dfadcc42fc6f9ac
jenifferferrasz03@gmail.com <deividferraz20@gmail.com>
2023-08-17 17:28:02 -0300

    jddheheh

--- python.py
+++ python.py
@@ -23,6 +23,6 @@ print('nasceu no dia', dia, 'do mes', mês, 'do ano de', ano )
 
 
 primeiro= input('Primeiro numero: ')
-segundo= input('Segundo num: ')
+segundo= input('Segundo numero: ')
 print(primeiro,segundo)
 
