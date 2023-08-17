6df969d05d695c381e8d15b5b8634b2c64096c83
jenifferneves <136993214+jenifferneves@users.noreply.github.com>
2023-08-17 20:15:51 -0300

    Update cursoex1.py

--- cursoex1.py
+++ cursoex1.py
@@ -1,2 +1,6 @@
 n=int(input('Digite um numero:'))
 print('Analisando o valor {}, seu antecessor é {} e o seu sucessor é {}'.format(n, (n-1), (n+1)))
+
+n=str(input('Digite seu nome:'))
+print('{} usted es amor de mi vida'.format(n)
+      
